a2ca7bcd5d53254d802ddb6f5473436b022ae987
Schartner <Justin.Schartner@kaldewei.com>
2022-05-06 17:35:32 +0200

    test

---
+++ test.py
@@ -0,0 +1,63 @@
+import torch
+import urllib
+from PIL import Image
+from torchvision import transforms
+
+model = None
+categories = None
+
+def guess():
+    global model
+    
+    input_image = Image.open("sample.jpg")
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]
+                             )
+    ])
+
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+
+    output = None
+        
+    with torch.no_grad():
+        output = model(input_batch)
+
+    probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+    top5_prob, top5_catid = torch.topk(probabilities, 5)
+
+    for i in range(top5_prob.size(0)):
+        print(categories[top5_catid[i]], top5_prob[i].item())
+
+def download(url1):
+    url, filename = (url1, "sample.jpg")
+    try: urllib.URLopener().retrieve(url, filename)
+    except: urllib.request.urlretrieve(url, filename)
+
+if __name__ == "__main__":
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg11', pretrained=True)
+    model.eval()
+
+    if torch.cuda.is_available():
+        print('cude')
+        model.to('cuda')
+
+    with open("imagenet_classes.txt", "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+                                       
+    while True:
+        inp = input()
+        
+        if inp == "q":
+            break
+
+        download(inp)
+        guess()
