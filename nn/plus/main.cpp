b21ae3b36927a341b54bb2bf97f69a0f022842d8
Justin Schartner <justin.schartner@tu-dortmund.de>
2022-06-15 07:54:16 +0200

    typo

--- nn/plus/main.cpp
+++ nn/plus/main.cpp
@@ -8,31 +8,37 @@
 
 std::tuple<std::vector<Matrix<float>>, std::vector<Matrix<float>>>get_data(int range) {
 
-  std::vector<Matrix<float>> x_train;
-  std::vector<Matrix<float>> y_train;
+    std::vector<Matrix<float>> x_train;
+    std::vector<Matrix<float>> y_train;
 
-  for(int i=0;i<range;i++) {
-    float x = randf(-50.0, 50.0);
-    float y = x*x;
+    float x_min = -50.0;
+    float x_max = 50.0;
 
-    x_train.push_back((Matrix<float>) ((std::vector<float>) {x}));
-    y_train.push_back((Matrix<float>) ((std::vector<float>) {y}));
-  }
+    float y_min = x_min*x_min;
+    float y_max = x_max*x_max;
+
+    for(int i=0;i<range;i++) {
+        float x = randf(-50.0, 50.0) / (x_max - x_min);
+        float y = x*x ;
+
+        x_train.push_back((Matrix<float>) ((std::vector<float>) {x}));
+        y_train.push_back((Matrix<float>) ((std::vector<float>) {y}));
+    }
   
-  return std::make_tuple(x_train, y_train);
+    return std::make_tuple(x_train, y_train);
 }
 
 std::vector<Matrix<float>>make_data(std::vector<float> vec) {
-  std::vector<Matrix<float>> result;
+    std::vector<Matrix<float>> result;
 
-  for(int i=0;i<vec.size();i++) {
-    Matrix<float> temp = ((std::vector<float>) {
+    for(int i=0;i<vec.size();i++) {
+        Matrix<float> temp = ((std::vector<float>) {
 	vec[i]});
 
-    result.push_back(temp);
-  }
+        result.push_back(temp);
+    }
 
-  return result;
+    return result;
 }
 
 void test_data(std::vector<Matrix<float>> x_train,
@@ -49,37 +55,32 @@ void test_data(std::vector<Matrix<float>> x_train,
 }
 
 int main() {
-  auto [x_train, y_train] = get_data(1000);
+    auto [x_train, y_train] = get_data(10000);
 
-  FcLayer dense1(1, 8);
-  ActivationLayer act1(relu, relu_prime);
-  FcLayer dense2(8, 8);
-  ActivationLayer act2(relu, relu_prime);
-  FcLayer dense3(8, 1);
+    FcLayer dense1(1, 10);
+    ActivationLayer act1(relu, relu_prime);
+    FcLayer dense2(10, 1);
 
-  Network net;
-  net.add_layer(&dense1);
-  net.add_layer(&act1);
-  net.add_layer(&dense2);
-  net.add_layer(&act2);
-  net.add_layer(&dense3);
+    Network net;
+    net.add_layer(&dense1);
+    net.add_layer(&act1);
+    net.add_layer(&dense2);
 
-  net.use(mse, mse_prime);
+    net.use(mse, mse_prime);
+  
+    //LOG
+    test_data(x_train, y_train, &net);
+    net.train(x_train, y_train, 300, 0.0001, true);
+    test_data(x_train, y_train, &net);
 
-  //LOG
-  test_data(x_train, y_train, &net);
-  net.train(x_train, y_train, 1500, 0.01, true);
-  test_data(x_train, y_train, &net);
-  if(true) return 0;
+    //TEST
+    auto list = make_data({4, -1, 11, 20, 8, -5});
 
-  //TEST
-  auto list = make_data({4, -1, 11, 20, 8, -5});
+    for(int i=0;i<list.size();i++) {
+        auto r = net.predict(list[i].scale(1.0/100.0)).scale(100.0);
 
-  for(int i=0;i<list.size();i++) {
-    auto r = net.predict(list[i]);
-
-    std::cout << list[i] << " -> " << r << std::endl;
-  }
+        std::cout << list[i] << " -> " << r << std::endl;
+    }
  
-  return 0;
+    return 0;
 }
--- nn/plus/src/fclayer.hpp
+++ nn/plus/src/fclayer.hpp
@@ -10,10 +10,6 @@ private:
     Matrix<float> input;
     Matrix<float> output;
 public:
-    /*
-[ [-1.70079e+06, -1.13071e+06, -1.77472e+06, -1.29577e+06, -1.56377e+06, -1.00939e+06, -1.42073e+06, -785381 ] ]
-[ [0.813837, 0.0466323, 0.814203, 0.239998, 0.897885, 0.256508, 0.269356, 0.208014 ] ]
-     */
 
     FcLayer(int input_size, int output_size):
         weights(input_size, output_size),
@@ -21,9 +17,9 @@ public:
         input(0, 0),
         output(0,0)
     {
-        weights = weights.for_each([](float n) -> float { return randf(0.0, 1.0);});
-        //weights = weights.scale(std::sqrt(2/output_size));
-        bias = bias.for_each([](float n) -> float { return randf(-1.0, 1.0);});
+        weights = weights.for_each([](float n) -> float { return randf(-1.0, 1.0);});
+        weights = weights.scale(std::sqrt(2.0/(output_size)));
+        bias = bias.for_each([](float n) -> float { return randf(0.0, 1.0);});
     };
   
     Matrix<float> forward_propagation(Matrix<float> _input) {
