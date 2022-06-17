8add12041ebd9f1c3967b72e185e4aedd888ff10
Justin Schartner <justin.schartner@tu-dortmund.de>
2022-06-17 07:51:06 +0200

    tanhh

--- nn/plus/main.cpp
+++ nn/plus/main.cpp
@@ -6,21 +6,29 @@
 #include ".\src\losses.hpp"
 #include ".\src\network.hpp"
 
+float x_min = -50.0;
+float x_max = 60.0;
+float y_min = 0;
+float y_max = x_max*x_max;
+
+float normalize(float x) {
+    return (x - x_min) / (x_max - x_min);
+}
+
+float denormalize(float x) {
+    return (x * (x_max - x_min)) + x_min;
+}
+
 std::tuple<std::vector<Matrix<float>>, std::vector<Matrix<float>>>get_data(int range) {
 
     std::vector<Matrix<float>> x_train;
     std::vector<Matrix<float>> y_train;
 
-    float x_min = -50.0;
-    float x_max = 50.0;
-
-    float y_min = x_min*x_min;
-    float y_max = x_max*x_max;
-
     for(int i=0;i<range;i++) {
-        float x = randf(-50.0, 50.0) / (x_max - x_min);
-        float y = x*x ;
-
+        float x = randf(x_min, x_max);
+        x = normalize(x);
+        float y = x*x;
+        
         x_train.push_back((Matrix<float>) ((std::vector<float>) {x}));
         y_train.push_back((Matrix<float>) ((std::vector<float>) {y}));
     }
@@ -47,18 +55,56 @@ void test_data(std::vector<Matrix<float>> x_train,
     for(int i=0;i<10;i++) {
         auto result = (*net).predict(x_train[i]);
         std::cout << x_train[i] << " => " << y_train[i] << " != " << result << std::endl;
-        auto err = relu_prime(mse_prime(y_train[i], result));
+        auto err = mse_prime(y_train[i], result);
         std::cout << "\t" << err << "\n";
     }
     std::cout << std::endl << std::endl;
     std::cout << (*net) << "\n";
 }
 
+float square(float n,float m) {
+    return n*m;
+}
+
 int main() {
-    auto [x_train, y_train] = get_data(10000);
+    float a = randf(x_min, x_max);
+    float b = a*a;
+    float ac = normalize(a);
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << denormalize(ac*ac) << std::endl;
+
+
+    return 0;
+}
+
+int main11() {
+    auto list = make_data({4.0, -1.0, 11.0, 20.0, 8.0, -5.0});
+
+    for(int i=0;i<list.size();i++) {
+        std::cout << list[i] << std::endl;
+        std::cout << list[i].for_each(list[i], square).to_string() << std::endl;
+
+        auto curr = list[i].for_each(normalize);
+        
+        std::cout << curr << std::endl;
+        std::cout << curr.for_each(curr, square) << std::endl;
+        std::cout << curr.for_each(curr, square).for_each(denormalize) << std::endl;
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
+int main1() {
+    
+    std::srand(std::time(0));
+    
+    auto [x_train, y_train] = get_data(5000);
 
     FcLayer dense1(1, 10);
-    ActivationLayer act1(relu, relu_prime);
+    ActivationLayer act1(tanhh, tanhh_prime);
     FcLayer dense2(10, 1);
 
     Network net;
@@ -70,16 +116,19 @@ int main() {
   
     //LOG
     test_data(x_train, y_train, &net);
-    net.train(x_train, y_train, 300, 0.0001, true);
+    net.train(x_train, y_train, 100, 0.001, true);
     test_data(x_train, y_train, &net);
 
     //TEST
-    auto list = make_data({4, -1, 11, 20, 8, -5});
+    auto list = make_data({4.0, -1.0, 11.0, 20.0, 8.0, -5.0});
 
     for(int i=0;i<list.size();i++) {
-        auto r = net.predict(list[i].scale(1.0/100.0)).scale(100.0);
+        auto current = list[i].for_each(normalize);
+        //auto current = list[i];
+        auto r = net.predict(current);
 
-        std::cout << list[i] << " -> " << r << std::endl;
+        std::cout << current << " -> " << r << std::endl;
+        std::cout << list[i] << " -> " << r.for_each(denormalize) << std::endl;
     }
  
     return 0;
